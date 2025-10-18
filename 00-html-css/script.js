15a1cc932eb30609082d86a9d7c9d80b7dbd958c
Andrea Duarte <ajduarteg@uamv.edu.ni>
2025-10-17 19:25:02 -0600

    sesion 2 y 3

--- 00-html-css/script.js
+++
@@ -1,7 +0,0 @@
-const boton = document.querySelector('#boton-importante');
-
-botonImportante.addEventListener("click", function() {
-    boton.textContent = "¡Aplicado!";
-    boton.style.backgroundColor = green;
-    boton.disabled = true;
-});
